dc74abd631b2a5448231952e1a33631172654f27
Denis <procommerz@users.noreply.github.com>
2018-05-26 18:28:56 +0200

    Update helper.rb

--- lib/indicators/calculations/helper.rb
+++ lib/indicators/calculations/helper.rb
@@ -71,22 +71,29 @@ end
 # Extra methods for mathematical calculations.
 module Enumerable
 
-  def sum
-    return self.inject(0){|accum, i| accum + i }
+  unless method_defined?(:sum)
+    def sum
+      return self.inject(0){|accum, i| accum + i }
+    end
+  end  
+
+  unless method_defined?(:mean)  
+    def mean
+      return self.sum / self.length.to_f
+    end    
+  end  
+  
+  unless method_defined?(:sample_variance)  
+    def sample_variance
+      m = self.mean
+      sum = self.inject(0){|accum, i| accum + (i - m) ** 2 }
+      return sum / (self.length - 1).to_f
+    end
   end
 
-  def mean
-    return self.sum / self.length.to_f
-  end
-
-  def sample_variance
-    m = self.mean
-    sum = self.inject(0){|accum, i| accum + (i - m) ** 2 }
-    return sum / (self.length - 1).to_f
-  end
-
-  def standard_deviation
-    return Math.sqrt(self.sample_variance)
-  end
-
+  unless method_defined?(:standard_deviation)  
+    def standard_deviation
+      return Math.sqrt(self.sample_variance)
+    end    
+  end    
 end
